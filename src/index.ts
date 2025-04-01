00cddc3b43e7a5221b7448c4ebcbb81a15664a4c
Field Palmer <gfieldpalmer@gmail.com>
2025-04-01 00:19:40 -0600

    AI functionality added to chatbot through LangChain/OpenAI

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,54 @@
 import 'dotenv/config';
 import readline from 'readline';
 
-console.log('Hello world!');
-console.log(process.env.OPENAI_API_KEY);
+// console.log('Hello world!');
+// console.log(process.env.OPENAI_API_KEY);
 
 const rl = readline.createInterface({
      input: process.stdin,
      output: process.stdout
 });
 
+// const chat = () => {
+//      rl.question('Enter a command (type "exit" to quit): ', (input) => {
+//           if (input.toLowerCase() === 'exit') {
+//                console.log('Goodbye!');
+//                rl.close();
+//           } else {
+//                console.log(`You entered: ${input}`);
+//                chat();
+//           }
+//      });
+// };
+
+// chat();
+
+import { SystemMessage, HumanMessage } from '@langchain/core/messages';
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+
+const model = new ChatOpenAI();
+
+const askModel = async (input: string) => {
+     const prompt = ChatPromptTemplate.fromMessages([new SystemMessage("You're a helpful assistant"), new HumanMessage(input)]);
+
+     const parser = new StringOutputParser();
+     const chain = prompt.pipe(model).pipe(parser);
+
+     return await chain.invoke(input);
+};
+
 const chat = () => {
-     rl.question('Enter a command (type "exit" to quit): ', (input) => {
+     rl.question('Enter a command (type "exit" to quit): ', async (input) => {
           if (input.toLowerCase() === 'exit') {
                console.log('Goodbye!');
                rl.close();
           } else {
-               console.log(`You entered: ${input}`);
+               const result = await askModel(input);
+
+               console.log(result);
+
                chat();
           }
      });
